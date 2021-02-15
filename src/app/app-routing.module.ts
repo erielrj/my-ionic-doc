333ddf9d0ac5bdd994326fb756e6afdc36f10ce8
erielrj <eriel.rj@gmail.com>
2021-02-14 23:41:37 -0300

    Navegação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'back',
+    loadChildren: () => import('./pages/back/back.module').then( m => m.BackPageModule)
   }
 ];
 
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    // Injetando NavController para Navegar entre as rotas
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
+  }
+
+  navToBackPage(){
+    this.navCtrl.navigateForward(['back'])
   }
 
 }
